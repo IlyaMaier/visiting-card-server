bc9196cbf1afb13ed096ae86775766c92c3417dd
Ilya <veershtein@gmail.com>
2018-11-30 00:48:43 +0100

    GCI 2018 POST and GET Commands

--- views/api.js
+++ views/api.js
@@ -7,3 +7,14 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
+
+router.post('/card', (req, res) => {
+  req.on('data', (data) => {
+    console.log('received');
+  });
+  res.send('0');
+});
+
+router.get('/card', (req, res) => {
+  res.send('{"name":"Example","number":"0123456789", "email":"example@email.com","github":"https://github.com"}');
+});
